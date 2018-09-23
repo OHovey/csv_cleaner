649ee47e2f084871442b08d88a12245fe7d15896
OHovey <30669691+OHovey@users.noreply.github.com>
2018-09-23 01:56:59 +0100

    Update __main__.py

--- csv_cleaner/csv_cleaner/__main__.py
+++ csv_cleaner/csv_cleaner/__main__.py
@@ -5,15 +5,11 @@ import os
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         c = CsvClean()
-        print('printed option 1')
     elif len(sys.argv) < 3 and '.csv' not in sys.argv[1] or len(sys.argv) < 3 and 'xlsx' not in sys.argv[1]:
         c = CsvClean(sys.argv[1])
-        print('printed option 2')
     elif len(sys.argv) < 3 and '.csv' in sys.argv[1] or len(sys.argv) < 3 and '.xlsx' in sys.argv[1]:
         c = CsvClean(file = sys.argv[2])
-        print('printed option 3')
     else:
         c = CsvClean(sys.argv[1], sys.argv[2])
-        print('printed option 4')
     
     c.clean()
